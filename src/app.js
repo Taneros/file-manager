65db329298b49f7988de886c072926b66df59787
rfathullin <rfathullin@luxms.com>
2023-04-18 18:53:45 +0300

    feat: add readDir

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import readline from 'readline';
-
-process.stdin.setEncoding('utf8')
-process.stdin.resume
-
 import os from 'os'
 import fs from 'fs';
+import {stat} from 'fs/promises'
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+process.stdin.setEncoding('utf8')
+// process.stdin.resume
+
+const scriptPath=fileURLToPath(import.meta.url)
+
+const dirName=path.dirname(scriptPath)
+
+const pathToReadFile=path.join(dirName)
 
 const homeDir=os.homedir()
 
-const app = async ( ) => {
+
+const app=async () => {
   
   const args = process.argv.slice(2).toString().split('=')[1]
   
@@ -16,20 +25,32 @@ const app = async ( ) => {
   
   console.log(`You are currently in path_to_working_directory, ${homeDir}!`)
 
+  console.log(`app.js - line: 28 ->> dirName`, dirName)
+
   process.stdin.on('data',(data) => {
-    console.log(`app.js - line: 20 ->> data`, data)
+    console.log(`app.js - line: 20 ->> data`,data.split('\n'))
+    data = data.split('\n')[0]
     
     switch (data) {
+      
+      // case /ls/.test(data):
       case 'ls':
         
-        fs.readdir(homeDir,(err, files) => {
+        fs.readdir(homeDir,(err,files) => {
           if(err) {
-            console.log(`app.js - line: 26 ->> err ls`, err)
+            console.log(`app.js - line: 26 ->> err ls`,err)
           }
 
-          files.forEach(element => {
-            console.log(`app.js - line: 30 ->> file`, element)
-          });
+          files=files.map(async (file,idx) => {
+            
+            const isDir = (await stat(homeDir)).isDirectory()
+
+            return ({
+              Name: file,
+              Type: isDir ? 'directory' : 'file'
+            })
+          })
+
         })
         
         break;
