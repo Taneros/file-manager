4e478fc5f691e827c7a2ca57c706a01043038521
Taneros <taneros@duck.com>
2023-04-25 00:24:21 +0300

    fix: modules & cd

--- src/cd.js
+++ src/cd.js
@@ -2,7 +2,7 @@ import path from 'path';
 
 export const cd = (homeDir, args) => {
 
-  const changeDir = path.join( homeDir, args[0] )
+  const changeDir = path.resolve( homeDir, args[0] )
 
   console.log( '\nCurrent directory is ', changeDir, '\n' )
 
